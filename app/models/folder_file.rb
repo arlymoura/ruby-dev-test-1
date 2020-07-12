70aaf841ddea8bc4d10f6c5e2e10e1e300eca685
Arly Moura <arlymoura@gmail.com>
2020-07-12 12:07:34 -0300

    add active storage and validate name file

--- app/models/folder_file.rb
+++ app/models/folder_file.rb
@@ -1,3 +1,7 @@
 class FolderFile < ApplicationRecord
   belongs_to :folder
+  has_one_attached :file
+
+  validates :name, uniqueness: { case_sensitive: false, scope: :folder,
+    message: 'Já existe um Arquivo com esse nome' }
 end
